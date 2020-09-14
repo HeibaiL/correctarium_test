0d4f6a87ff13d8fa55bdee61db14a18695fb048b
vladyslavdubyna <vladikd96@gmail.com>
2020-09-14 13:45:59 +0300

    "Fix for deadline calculator"

--- src/components/SubmitOrder.js
+++ src/components/SubmitOrder.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-import {parseNumWithComma} from "../helpers";
+import {parseNumWithComma, parseDate, addMinutes} from "../helpers";
 import CONSTANTS from "../constants";
 import "../styles/submitorder.scss"
 
+const {START, END} = CONSTANTS.WORK_HOURS;
+
+const resetDate = (date, inst) => {
+    const hoursToAdd = date.getHours() - END >= 0 ? date.getHours() - END : date.getHours() + 24 - END;
+
+    const newDate = new Date(date.getTime());
+
+    newDate.setDate(date.getDate() + (inst ? 0 : 1));
+    newDate.setHours(10 + hoursToAdd, 30);
+    if (newDate.getHours() >= END) {
+        return resetDate(newDate)
+    }
+    return newDate
+}
+
 const SubmitOrder = ({length, language}) => {
+    const [current, setCurrent] = useState({});
+
+    const minLength = language && CONSTANTS[language].TEXT_LENGTH.FOR_TIME;
+
+    useEffect(() => {
+        setCurrent(new Date());
+    }, [])
+
 
     const calculatePrice = (length, language) => {
         if (!length || !language) {
@@ -12,26 +35,37 @@ const SubmitOrder = ({length, language}) => {
         }
         let price = CONSTANTS[language].PRICE.MIN;
         let perPage = CONSTANTS[language].PRICE.MIN / CONSTANTS[language].TEXT_LENGTH.FOR_PRICE;
-        let payedLength = length - CONSTANTS[language].TEXT_LENGTH.FOR_PRICE;
+        let leftLength = length - CONSTANTS[language].TEXT_LENGTH.FOR_PRICE;
         if (length > 1000) {
-            price = price + perPage * payedLength;
+            price = price + perPage * leftLength;
         }
         return parseNumWithComma(price);
     }
-    const calculateTime = (length, language) => {
-        if (!length || !language) {
-            return ""
-        }
-        let str = "Здамо за: ";
-        // if (length <= CONSTANTS[language].TEXT_LENGTH.FOR_TIME) {
-        //     str = str + "1 годину"
-        // } else if (length <= CONSTANTS[language].TEXT_LENGTH.FOR_PRICE * 2) {
-        //     str = str + "2 години"
-        // } else if (length <= CONSTANTS[language].TEXT_LENGTH.FOR_PRICE * 3) {
-        //     str = str + "3 години"
-        // }
-        return str;
 
+    const calculateTime = (length, language) => {
+        if (!length || !language || !current) {
+            return ""
+        };
+
+        const minsToAdd = Math.ceil(length / minLength) * 60;
+        let newDate = new Date();
+
+        if (length <= minLength) {
+            newDate = addMinutes(current, 60);
+        } else if (length > minLength) {
+            newDate = addMinutes(current, minsToAdd + 30)
+        }
+
+        if (newDate.getHours() >= END) {
+            newDate = resetDate(newDate)
+        } else if (newDate.getHours() < START) {
+            newDate = resetDate(newDate, "foo")
+        }
+
+        let addTime = newDate.getTime() - current.getTime();
+
+        let {date, time} = parseDate(newDate);
+        return `${date} о ${time}`;
     }
 
     return <div className="submit_order">
@@ -44,7 +78,8 @@ const SubmitOrder = ({length, language}) => {
             </span>
         </div>
         <div className="estimated_time">
-            {calculateTime(length, language)}
+            <span> {language && length && "Термін виконання"} </span>
+            <span>{calculateTime(length, language)}</span>
         </div>
         <button className="submit_button">
             Замовити
--- src/helpers.js
+++ src/helpers.js
@@ -12,3 +12,18 @@ export const parseNumWithComma = num => {
     }
     return value;
 };
+
+export const parseDate = Date => {
+    const dd = String(Date.getDate()).padStart(2, '0');
+    const mm = String(Date.getMonth() + 1).padStart(2, '0');
+    const yyyy = Date.getFullYear();
+    const date = mm + '.' + dd + '.' + yyyy;
+    const hours = Date.getHours().toString().length < 2 ? "0" + Date.getHours() : Date.getHours();
+    const minutes = Date.getMinutes().toString().length < 2 ? "0" + Date.getMinutes() : Date.getMinutes();
+    const time = hours + ":" + minutes;
+    return {date, time};
+}
+
+export function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes * 60000);
+}
